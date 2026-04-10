a6b34dd350c978d9f54d31ed8591ea7a423acab7
Yeshey <yesheysangpo@gmail.com>
2026-04-10 23:30:41 +0100

    weird controls

--- player___character_body_2d.gd
+++ player___character_body_2d.gd
@@ -1,22 +1,23 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+func _physics_process(delta):
+	# Add gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Handle Jump (W or Space)
+	if Input.is_action_just_pressed("ui_accept") or Input.is_key_pressed(KEY_W):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get WASD/Arrow input
+	var direction = Input.get_axis("ui_left", "ui_right") 
+	# Note: Godot maps A/D and Left/Right to "ui_left/right" by default!
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
